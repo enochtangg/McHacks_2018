60069a58724a772fb7dfc0f3da62ae9c37bd40cc
enochtangg <enochtangg@gmail.com>
2018-02-03 17:56:56 -0500

    authenticator

--- src/app/services/watsonService.service.ts
+++ src/app/services/watsonService.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 
 @Injectable()
 export class WatsonService {
-  var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
-  var tone_analyzer = new ToneAnalyzerV3({
-  username: '••••••••••••••••••••••••••••••••••••',
-  password: '••••••••••••',
-  version_date: '2017-09-21',
-  headers: {
-    'X-Watson-Learning-Opt-Out': 'true'
+  ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+  tone_analyzer = new ToneAnalyzerV3({
+    username: '••••••••••••••••••••••••••••••••••••',
+    password: '••••••••••••',
+    version_date: '2017-09-21',
+    headers: {
+      'X-Watson-Learning-Opt-Out': 'true'
+    }
+  });
+  params = {
+    'tone_input': require('tone.json'),
+    'content_type': 'application/json'
+  };
+  postWatson() {
   }
-});
 }
