0be4540d46db229ee87ab6ccba9c2eeb2e19545c
Aaditya Sondhi <a6sondhi@gmail.com>
2018-02-03 17:58:14 -0500

    added post watson method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WatsonService } from './services/watsonService.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   input: string = '';
+
+  constructor( watsonService: WatsonService ) { }
+
+  postWatson(body: strig) {
+    this.watsonService.postWatson()
+  }
+
+  onClickSubmit() {
+
+  }
 }
